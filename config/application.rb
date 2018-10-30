e0fde58cba08d757a5d264968ed495908281d5c8
kajiayano <ayanyano428@gmail.com>
2018-10-30 18:37:16 +0900

    delete frozen 01

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,12 +13,12 @@ module Potepanec
 
     config.to_prepare do
       # Load application's model / class decorators
-      Dir.glob(File.join(File.dirname(__FILE__), '../app/**/*_decorator*.rb')) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
 
       # Load application's view overrides
-      Dir.glob(File.join(File.dirname(__FILE__), '../app/overrides/*.rb')) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end

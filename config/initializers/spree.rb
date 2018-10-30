bb16add361a595f18d6aaf1bf3507aebd237dae7
kajiayano <ayanyano428@gmail.com>
2018-10-30 20:49:38 +0900

    edit rubocop01

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,6 +18,7 @@ Spree.config do |config|
   # any inventory changes.
   # config.inventory_cache_threshold = 3
 
+
   # Frontend:
 
   # Custom logo for the frontend
@@ -25,6 +26,7 @@ Spree.config do |config|
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
+
 
   # Admin:
 

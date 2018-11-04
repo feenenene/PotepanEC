500940485f9b6154bc2a673d800a4d469c6d274f
kajiayano <ayanyano428@gmail.com>
2018-11-04 23:47:42 +0900

    add cart page rspec

--- spec/controllers/potepan/products_controller_spec.rb
+++ spec/controllers/potepan/products_controller_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe Potepan::ProductsController, type: :controller do
       get :show, params: { id: product.id }
     end
 
-    it "responds successfully" do
+    scenario "responds successfully" do
       expect(response).to be_successful
       expect(response).to have_http_status "200"
     end
 
-    it "assigns @product" do
+    scenario "assigns @product" do
       expect(assigns(:product)).to eq product
     end
 
-    it "render show page" do
+    scenario "render show page" do
       expect(response).to render_template "potepan/products/show"
     end
   end
--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -3,11 +3,17 @@ RSpec.feature "Products", :type => :feature do
   describe "display product details" do
     let(:product) { create(:product) }
 
-    it "display each product details" do
-      visit "potepan/products/#{product.id}"
+    background { visit potepan_product_path(product.id) }
+
+    scenario "display each product details" do
       expect(page).to have_content "#{product.name}"
       expect(page).to have_content "#{product.display_price}"
       expect(page).to have_content "#{product.description}"
+    end
+
+    scenario 'move to cart page' do
+      click_link 'カートへ入れる'
+      expect(page).to have_current_path potepan_cart_page_path
     end
   end
 end

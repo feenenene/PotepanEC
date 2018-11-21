ad5a610a93b787262e066807922c8dfd9cccbf76
kajiayano <ayanyano428@gmail.com>
2018-11-21 13:06:33 +0900

    edit product spec and head

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
-RSpec.feature "Products", :type => :feature do
+
+RSpec.feature "Products", type: :feature do
   describe "display product details" do
     let(:product) { create(:product) }
 
     background { visit potepan_product_path(product.id) }
 
     scenario "display each product details" do
-      expect(page).to have_content "#{product.name}"
-      expect(page).to have_content "#{product.display_price}"
-      expect(page).to have_content "#{product.description}"
+      expect(page).to have_content product.name
+      expect(page).to have_content product.display_price
+      expect(page).to have_content product.description
     end
 
     scenario 'move to cart page' do

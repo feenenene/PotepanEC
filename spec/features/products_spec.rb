0e888470d061523962dafa225e0205baedc72a09
kajiayano <ayanyano428@gmail.com>
2018-11-22 21:10:27 +0900

    edit product spec and application helper

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 RSpec.feature "Products", type: :feature do
   describe "display product details" do
-    let(:product) { create(:product) }
+    given(:product) { create(:product) }
 
     background { visit potepan_product_path(product.id) }
 
     scenario "display each product details" do
-      expect(page).to have_content product.name
-      expect(page).to have_content product.display_price
+      expect(page).to have_title "#{product.name} - Potepan Store"
+      expect(page).to have_selector '.page-title h2', text: product.name
+      expect(page).to have_selector '.col-xs-6 li', text: product.name
+      expect(page).to have_selector '.media-body h2', text: product.name
+      expect(page).to have_selector 'h3', text: product.display_price
       expect(page).to have_content product.description
     end
 
